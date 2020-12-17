d6c41f5dfda352d059424f4bfdd417dec74b2c0b
nikiul <nikulans@gmail.com>
2020-12-17 16:11:50 +0300

    added responsive room fund

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,48 @@
+//about-sanatorium
+$(document).on('click','.js-videoPoster',function(e) {
+  //отменяем стандартное действие button
+  e.preventDefault();
+  var poster = $(this);
+  // ищем родителя ближайшего по классу
+  var wrapper = poster.closest('.js-videoWrapper');
+  videoPlay(wrapper);
+});
+
+//вопроизводим видео, при этом скрывая постер
+function videoPlay(wrapper) {
+  var iframe = wrapper.find('.js-videoIframe');
+  // Берем ссылку видео из data
+  var src = iframe.data('src');
+  // скрываем постер
+  wrapper.addClass('videoWrapperActive');
+  // подставляем в src параметр из data
+  iframe.attr('src',src);
+};
+
+$(document).ready(function(){
+  $("#owl-carousel__about-sanatorium").owlCarousel({
+      margin: 10,
+      loop: true,
+      nav: true,
+      pagination: true,   
+      dots: true,
+      navText : ["",""],
+      responsiveClass:true,
+      responsiveBaseElement:"body",
+      responsive:{
+          0: {
+              item:1.05
+          },
+          320:{
+              items:1.5
+          },
+          604: {
+              items: 2,  
+          }
+      }
+  });
+});
+
 //fix hover element on mobile
 let allEl = document.querySelectorAll('*')
 for(let i = 0; i < allEl.length; i++){
@@ -130,6 +175,7 @@ $(document).ready(function(){
 });
 
 
+//nav
 //infra
 jQuery(($) => {
 
@@ -152,57 +198,6 @@ jQuery(($) => {
     });
 
 });
-//nav
-//sanatorium-progs
-//about-sanatorium
-$(document).on('click','.js-videoPoster',function(e) {
-  //отменяем стандартное действие button
-  e.preventDefault();
-  var poster = $(this);
-  // ищем родителя ближайшего по классу
-  var wrapper = poster.closest('.js-videoWrapper');
-  videoPlay(wrapper);
-});
-
-//вопроизводим видео, при этом скрывая постер
-function videoPlay(wrapper) {
-  var iframe = wrapper.find('.js-videoIframe');
-  // Берем ссылку видео из data
-  var src = iframe.data('src');
-  // скрываем постер
-  wrapper.addClass('videoWrapperActive');
-  // подставляем в src параметр из data
-  iframe.attr('src',src);
-};
-
-$(document).ready(function(){
-  $("#owl-carousel__about-sanatorium").owlCarousel({
-      margin: 10,
-      loop: true,
-      nav: true,
-      pagination: true,   
-      dots: true,
-      navText : ["",""],
-      responsiveClass:true,
-      responsiveBaseElement:"body",
-      responsive:{
-          0: {
-              item:1.05
-          },
-          320:{
-              items:1.5
-          },
-          604: {
-              items: 2,  
-          }
-      }
-  });
-});
-//section-activity
-//section-discount
-//section-feedback
-//small-banner
-//under-hero
 //room-fund
 function openRoom(evt, buildingNumber) {
     var i, tabcontent, tablinks;
@@ -239,3 +234,9 @@ $(document).ready(function(){
         }
     });
   });
+//sanatorium-progs
+//section-activity
+//section-discount
+//section-feedback
+//small-banner
+//under-hero
