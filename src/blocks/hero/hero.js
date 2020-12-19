90b66ea54987905d40f879bf31f75c781d7349ee
Shakhrom <leon9208@mail.ru>
2020-12-19 05:28:55 +0300

    Modified hero carousel

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,47 +1,33 @@
-//hero
-// $(document).ready(function() {
-//     function checkWidth() {
-//       var windowWidth = $('body').innerWidth(),
-//           elem = $(".banner__blocks-row"); 
-//       if(windowWidth < 810){
-//         elem.addClass('owl-carousel');
-//         elem.addClass('owl-theme');
-//         // elem.attr('id', 'owl-carousel__banner');
-//       }
-//       else{
-//         elem.removeClass('owl-carousel');
-//         elem.removeClass('owl-theme');
-//         // elem.removeAttr('id', 'owl-carousel__banner');
-
-//       }
-//     }
-  
-//     checkWidth();
-  
-//     $(window).resize(function(){
-//       checkWidth();
-//     });
-// });
-
-if($(window).width() < 810){
-    $(document).ready(function() {
-        $("#owl-carousel__banner").owlCarousel({
-            margin: 10,
-            loop: true,
-            nav: true,
-            pagination: true,   
-            dots: true,
-            navText : ["",""],
-            responsiveClass:true,
-            responsiveBaseElement:"body",
-            responsive:{
-                0:{
-                    items:1.5
-                },
-                563: {
-                    items: 2  
-                }
+var heroCarousel = $("#owl-carousel__banner");
+function heroCarouselInit() {
+    heroCarousel.owlCarousel({
+        margin: 10,
+        loop: true,
+        nav: true,
+        pagination: true,   
+        dots: true,
+        navText : ["",""],
+        responsiveClass:true,
+        responsiveBaseElement:"body",
+        responsive:{
+            0:{
+                items:1.1
+            },
+            563: {
+                items: 2  
             }
-        });     
-    });   
+        }
+    });
 };
+$(document).ready(function(){
+    if($(window).width() < 810){
+        heroCarouselInit()
+    };
+    $(window).resize(function(){
+        if($(window).width() < 810){
+            heroCarouselInit()
+        }else{
+            heroCarousel.trigger('destroy.owl.carousel')
+        };  
+    })
+});
