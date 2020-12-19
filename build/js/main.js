d9b2655b53c2ef2008b8f2c31512e7da006a7b90
Shakhrom <leon9208@mail.ru>
2020-12-19 08:39:44 +0300

    build files

--- build/js/main.js
+++ build/js/main.js
@@ -19,9 +19,9 @@ function videoPlay(wrapper) {
   iframe.attr('src',src);
 };
 
-if($(window).width() < 811){
-  $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+var aboutCarousel = $("#owl-carousel__about-sanatorium");
+function aboutCarouselInit() {
+  aboutCarousel.owlCarousel({
         margin: 10,
         loop: true,
         nav: true,
@@ -31,129 +31,147 @@ if($(window).width() < 811){
         responsiveClass:true,
         responsiveBaseElement:"body",
         responsive:{
-            0: {
-                item:1.05
+            0:{
+                items:1.1
             },
-            320:{
-                items:1.5
-            },
-            604: {
-                items: 2,  
+            563: {
+                items: 2  
             }
         }
     });
-  });
 };
+$(document).ready(function(){
+    if($(window).width() < 810){
+      aboutCarouselInit()
+    };
+    $(window).resize(function(){
+        if($(window).width() < 810){
+          aboutCarouselInit()
+        }else{
+          aboutCarousel.trigger('destroy.owl.carousel')
+        };  
+    })
+});
+//START footer
+$("footer .title-nav-section").on('click', function () {
+  if ($(window).width() <= 1200) {
+    $(this).toggleClass('open');
+    $(this).next().slideToggle();
+  }
+});
 
-//fix hover element on mobile
-let allEl = document.querySelectorAll('*')
-for(let i = 0; i < allEl.length; i++){
-  allEl.ontouchstart = () => this.mouseover()
-  allEl.ontouchcancel = () => this.mouseover()
+$(window).resize(function () {
+  if ($(window).width() > 1200) {
+    $("footer .title-nav-section").removeClass('open');
+    $("footer .title-nav-section").next().fadeIn(0);
+  }
+});
+//END footer
+//header start
+$(document).ready(function(){
+    // десктопное меню
+    $('.header__menu').click(function(){
+        $('.header__desctop-menu').addClass('header__side-menu--active');
+    });
+    $('.header__desctop-burger--wrapper').click(function(){
+        $('.header__desctop-menu').removeClass('header__side-menu--active');
+    });
+    
+    // мобильное меню
+    $('.header__top-burger').click(function(){
+        $(this).find('.header__top-burger__menu-icon').toggleClass('header__top-burger__menu-icon--active');
+        $('.header__sub--wrapper').toggleClass('is-active')
+    });
+
+    // лочим прокрутку сайта при активном десктопном/мобильном меню
+    $('.header__menu, .header__top-burger').click(function() {
+        $('body').toggleClass('body__lock');
+    });
+    // разлочиваем прокрутку сайта при убраном десктопном/мобильном меню
+    $('.header__desctop-burger--wrapper, header__top-burger').click(function() {
+        $('body').removeClass('body__lock');
+    });
+
+    // выпадающий список телефонов в шапке и сайдбаре
+    var tel = $(".tel");
+    var telList = $('.tel__list');
+    var telBtn = $('.tel-btn');
+    var telCurrent = $('.tel-current__link');
+
+    telBtn.click(function(e){
+        telList.fadeToggle();
+        $(this).toggleClass('isactive')
+    });
+
+    telList.on('click', '.tel__item', function(){
+        var th = $(this);
+        var thText = th.text();
+        telCurrent
+        .text(thText)
+        .attr('href', 'tel:'+thText+'');
+        telList.fadeOut();
+        telBtn.removeClass('isactive');
+    });
+
+    $(document).mouseup(function (e){
+        if (!tel.is(e.target)
+        && tel.has(e.target).length === 0) {
+            telList.fadeOut();
+            telBtn.removeClass('isactive');
+        }
+    });
+
+    // фиксация шапки при скролле
+
+    var	sticky = $('.header__sub--wrapper')
+        pos = sticky.offset().top,
+        win = $(window);
+
+    win.on("scroll", function() {
+        if(win.width() > 960){
+            if(win.scrollTop() >= pos){
+                $('.header').addClass("fixed");
+            } else {
+                $('.header').removeClass("fixed");
+            }
+        }
+    });
+});
+
+//header end
+var heroCarousel = $("#owl-carousel__banner");
+function heroCarouselInit() {
+    heroCarousel.owlCarousel({
+        margin: 10,
+        loop: true,
+        nav: true,
+        pagination: true,   
+        dots: true,
+        navText : ["",""],
+        responsiveClass:true,
+        responsiveBaseElement:"body",
+        responsive:{
+            0:{
+                items:1.1
+            },
+            563: {
+                items: 2  
+            }
+        }
+    });
 };
-
-//Проверка поддержки WEBP
-function check_webp_feature(feature, callback) {
-  var kTestImages = {
-      lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
-      lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
-      alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
-      animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
-  };
-  var img = new Image();
-  img.onload = function () {
-      var result = (img.width > 0) && (img.height > 0);
-      callback(feature, result);
-  };
-  img.onerror = () => callback(feature, false);
-  img.src = "data:image/webp;base64," + kTestImages[feature];
-};
-//Если браузер поддерживает WEBP, то добавляем класс тегу body
-check_webp_feature('lossy', function (feature, isSupported) {
-  isSupported ? document.body.classList.add('webp-support-js') : document.body.classList.add('webp-nosupport-js')
+$(document).ready(function(){
+    if($(window).width() < 810){
+        heroCarouselInit()
+    };
+    $(window).resize(function(){
+        if($(window).width() < 810){
+            heroCarouselInit()
+        }else{
+            heroCarousel.trigger('destroy.owl.carousel')
+        };  
+    })
 });
-
-// Перекрашивание средней полоски в бургер меню при наведении на header__menu
-var bg = document.getElementById('menu-icon-line');
-document.getElementById('header__menu').addEventListener("mouseover", function(){bg.style.background = "#006CB5";this.addEventListener("mouseout", function(){bg.style.background = "";});
-});
-
-// бургер в десктопном меню
-const desctopBtn = document.querySelector('.header__menu');
-const desctopBtn2 = document.querySelector('.header__desctop-burger--wrapper');
-const desctopmenuIcon = document.querySelector('.header__desctop-burger__menu-icon');
-const desctopMenu = document.querySelector('.header__desctop-menu');
-
-// Клик по кнопке для скрытия / показа фильтра и изменения  иконки
-desctopBtn.onclick = function(){
-    desctopmenuIcon.classList.toggle('header__desctop-menu--active');
-    desctopMenu.classList.toggle('header__side-menu--active');
-};
-desctopBtn2.onclick = function(){
-    desctopmenuIcon.classList.toggle('header__desctop-menu--active');
-    desctopMenu.classList.toggle('header__side-menu--active');
-};
-
-// бургер в сайдбаре
-const sidebarToggleBtn = document.querySelector('.header__top-burger--wrapper');
-const menuIcon = document.querySelector('.header__top-burger__menu-icon');
-const sidebar = document.querySelector('.header__side-menu');
-
-// Клик по кнопке для скрытия / показа фильтра и изменения  иконки
-sidebarToggleBtn.onclick = function(){
-    menuIcon.classList.toggle('header__top-burger__menu-icon--active');
-    sidebar.classList.toggle('header__side-menu--active');
-};
-
-// добавляем Тень при активном боковом меню
-$('.header__top-burger').click(function() {
-    $('.header__side-menu').toggleClass('body-shadow');
-});
-
-// лочим прокрутку сайта при активном десктопном/мобильном меню
-$('.header__menu, .header__top-burger').click(function() {
-    $('body').toggleClass('body__lock');
-});
-// разлочиваем прокрутку сайта при убраном десктопном/мобильном меню
-$('.header__desctop-burger--wrapper, header__top-burger').click(function() {
-    $('body').removeClass('body__lock');
-});
-
-// выпадающий список в шапке и сайдбаре
-var tel = $(".tel");
-var telList = $('.tel__list');
-var telBtn = $('.tel-btn');
-var telCurrent = $('.tel-current__link');
-
-telBtn.click(function(e){
-	telList.fadeToggle();
-});
-
-telList.on('click', '.tel__item', function(){
-	var th = $(this);
-	var thText = th.text();
-	telCurrent
-		.text(thText)
-		.attr('href', 'tel:'+thText+'');
-		telList.fadeOut();
-});
-
-$(document).mouseup(function (e){
-	if (!tel.is(e.target)
-			&& tel.has(e.target).length === 0) {
-		telList.fadeOut(); // скрываем его
-	}
-});
-
-// скролл шапки
-window.onscroll = function showHeader() {
-    var header = document.querySelector('.header');
-    if(window.pageYOffset > 160) {
-        header.classList.add('header__fixed');
-    } else {
-        header.classList.remove('header__fixed');
-    }
-}
 //infra
 jQuery(($) => {
 
@@ -176,53 +194,6 @@ jQuery(($) => {
     });
 
 });
-//hero
-// $(document).ready(function() {
-//     function checkWidth() {
-//       var windowWidth = $('body').innerWidth(),
-//           elem = $(".banner__blocks-row"); 
-//       if(windowWidth < 810){
-//         elem.addClass('owl-carousel');
-//         elem.addClass('owl-theme');
-//         // elem.attr('id', 'owl-carousel__banner');
-//       }
-//       else{
-//         elem.removeClass('owl-carousel');
-//         elem.removeClass('owl-theme');
-//         // elem.removeAttr('id', 'owl-carousel__banner');
-
-//       }
-//     }
-  
-//     checkWidth();
-  
-//     $(window).resize(function(){
-//       checkWidth();
-//     });
-// });
-
-if($(window).width() < 810){
-    $(document).ready(function() {
-        $("#owl-carousel__banner").owlCarousel({
-            margin: 10,
-            loop: true,
-            nav: true,
-            pagination: true,   
-            dots: true,
-            navText : ["",""],
-            responsiveClass:true,
-            responsiveBaseElement:"body",
-            responsive:{
-                0:{
-                    items:1.5
-                },
-                563: {
-                    items: 2  
-                }
-            }
-        });     
-    });   
-};
 //nav
 //room-fund
 function openRoom(evt, buildingNumber) {
